5654beeb5a1603fb8452d46a9c83a7fab66bf925
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-02-03 10:41:18 -0300

    feat: Categoria

---
+++ src/main/java/com/lucascardoso/dominio_orm/DominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.lucascardoso.dominio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioOrmApplication.class, args);
+	}
+
+}
